a142a75a7cd5b1c68c825c44b1ff013c26e2a4f6
IsaacLar <I.g.larssen2@newcastle.ac.uk>
2026-06-11 14:13:37 +0100

    Added obstacles and camera following

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,4 @@
+using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -7,10 +8,12 @@ public class PlayerScript : MonoBehaviour
 
     public Rigidbody rb;
     public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
     //Player controls using new Input system
     public InputAction playerControls;
 
     private float moveDirection;
+    private bool alive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,20 +34,31 @@ public class PlayerScript : MonoBehaviour
     // FixedUpdate called when dealing with Physics
     void FixedUpdate()
     {
-        //Add forward force to player
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-        moveDirection = playerControls.ReadValue<float>();
-
-        if (moveDirection > 0)
+        if (alive)
         {
-            rb.AddForce(500 * Time.deltaTime, 0, 0);
-        }
+            //Add forward force to player
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (moveDirection < 0)
-        {
-            rb.AddForce(-500*Time.deltaTime, 0, 0);
+            moveDirection = playerControls.ReadValue<float>();
+
+            if (moveDirection > 0)
+            {
+                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            }
+
+            if (moveDirection < 0)
+            {
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            }
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            Debug.Log("Game Over!");
+            alive = false;
+        }
+    }
 }
